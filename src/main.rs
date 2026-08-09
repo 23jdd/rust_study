af395359ea9c80211aabdda9c234a4b781ac4385
long <ab1234_1123@qq.com>
2026-08-09 12:12:30 +0800

    Init

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::env;
+use std::fs;
+use std::io;
+
+
+
+fn read_all(path: &str) -> io::Result<String> {
+    let content = fs::read_to_string(path)?;
+    io::Result::Ok(content)
+}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        eprintln!("args number is not enough")
+    }
+    let file_name = &args[1];
+    let content = read_all(&file_name);
+    match content {
+        Ok(v) => {
+               println!("{}",v)
+        },
+        Err(_) => {
+            eprintln!("READ {} FAIL",file_name)
+        },
+    }
+    unsafe {
+         
+    }
+    
+}
